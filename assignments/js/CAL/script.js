ffbc0692c9eabfda731c18841e0f4b67ace52314
theek23 <theekshanadesilva28@gmail.com>
2023-10-09 04:48:11 +0530

    calculator assignment done

---
+++ assignments/js/CAL/script.js
@@ -0,0 +1,20 @@
+
+function appendToResult(value) {
+    clickedCount();
+    if (value=="=") {
+        calculate();
+    }else{
+        document.getElementById('result').textContent += value;
+    }
+}
+
+function clearResult() {
+    document.getElementById('result').textContent = '';
+}
+
+function calculate() {
+    var expression = document.getElementById("result").textContent;
+    var result = eval(expression);
+    document.getElementById('result').textContent = result;
+}
+
