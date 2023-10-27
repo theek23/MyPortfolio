9894f56a19fb85dcfe76c7f3e02ae23a31852f14
theek23 <theekshanadesilva28@gmail.com>
2023-10-27 23:38:29 +0530

    Loading page

---
+++ styles/style.js
@@ -0,0 +1,6 @@
+let loader = document.getElementById("loader");
+window.addEventListener("load", function () {
+    console.log("This message will print just after fully loading the website")
+    loader.style.display = 'none';
+    console.log("This message will print just after fully loading the website")
+});
